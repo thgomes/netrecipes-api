41552def5ee4e4a770d5c46e6f6ebd1425231a2f
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-05-23 19:24:57 -0300

    feature: user update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -18,9 +18,25 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'E-mail already registered' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({ id, name, email });
   }
 }
 
